2816e9a628217e8dd1e167e1f16596ac1bb54c47
Konstantin Solovyev <solovyev@yandex-team.ru>
2017-03-02 14:25:21 +0300

    object case#1

---
+++ Duplicates.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Реализовать clearDuplicates и clearSame. Первая убирает одинаковые объекты из списка, вторая убирает одни и те же объекты из списка.
+ */
+public class Duplicates {
+    
+    private final List<Object> bestListEver = new ArrayList();
+    
+    public void cleareDuplicates() {
+    //...
+    }
+    
+    public void clearSame() {
+    //...
+    }
+
+    public List<Object> getBestListEver() {
+        return bestListEver;
+    }
+}
