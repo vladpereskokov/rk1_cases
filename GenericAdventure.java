0926d54be38d9ef29a6e6c718fec55c5cea88f79
Vladislav Pereskokov <vladpereskokov@mail.ru>
2017-03-30 13:09:17 +0300

    Update GenericAdventure.java

--- GenericAdventure.java
+++ GenericAdventure.java
@@ -1,83 +1,91 @@
-
 //Помоги главнокомандующему справиться с войсками (поправить компиляцию, упростить, где можно, но трогать функцию main нельзя)
-public class GenericAdventure {
+  public static class GenericAdventure {
     public interface Man {
     }
 
     public static class Knight implements Man {
-        public void cry(){}
+      public void cry() {
+      }
     }
 
     public static class Peasant implements Man {
     }
 
-    public interface Army<T> {
-        void move();
+    public interface Army<T extends Man> {
+      T getCommander();
+
+      void move();
     }
 
-    public static class Avantgarde<T> implements Army<T> {
-        private T comander;
-        public Avantgarde(T comander) {
-            this.comander = comander;
-        }
-        public void fallBack() {}
-        public void move(){}
+    public static class Avantgarde<T extends Man> implements Army<T> {
+      private T commander;
+
+      public Avantgarde(T commander) {
+        this.commander = commander;
+      }
+
+      @Override
+      public T getCommander() {
+        return commander;
+      }
+
+      public void fallBack() {
+      }
+
+      public void move() {
+      }
     }
 
-    public static class Escort<T> implements Army<T> {
-        private T comander;
-        public Escort(T comander) {
-            this.comander = comander;
-        }
+    public static class Escort<T extends Man> implements Army<T> {
+      private T commander;
 
-        public void move(){}
+      public Escort(T commander) {
+        this.commander = commander;
+      }
+
+      @Override
+      public T getCommander() {
+        return commander;
+      }
+
+      public void move() {
+      }
     }
 
-
-    public static void doScout(Avantgarde<Man> avantgarde) {
-        avantgarde.move();
-        avantgarde.fallBack();
+    public static void doScout(Avantgarde<Knight> avantgarde) {
+      avantgarde.move();
+      avantgarde.fallBack();
     }
 
     public static void moveEscort(Escort<Man> escort) {
-        escort.move();
+      escort.move();
     }
 
-    public static void moveKnights(Avantgarde<Knight> avantgarde) {
-        avantgarde.comander.cry();
-        avantgarde.move();
+    public static void moveKnights(Army<Knight> avantgarde) {
+      avantgarde.getCommander().cry();
+      avantgarde.move();
     }
 
-    public static void moveKnights(Escort<Knight> escort) {
-        escort.comander.cry();
-        escort.move();
+    public static void moveSomething(Army<? extends Man> army) {
+      army.move();
     }
-
-    public static void moveSomething(Avantgarde<Peasant> army) {
-        army.move();
-    }
-
-    public static void moveSomething(Escort<Knight> army) {
-        army.move();
-    }
-
 
     public static void main(String[] args) {
-        final Avantgarde<Knight> knightsAvanLeft = new Avantgarde<>(new Knight());
-        final Army<Knight> knightsAvanRight = new Avantgarde<>(new Knight());
-        final Escort<Man> peasantsEscortBack = new Escort<>(new Peasant());
-        final Escort<Peasant> peasantEscortLeft = new Escort<>(new Peasant());
-        final Army<Peasant> peasantEscortRigth = new Escort<>(new Peasant());
-        final Army<Knight> knigthsReserve = new Escort<>(new Knight());
-        final Avantgarde<Man> raiders = new Avantgarde<>(new Knight());
+      final Avantgarde<Knight> knightsAvanLeft = new Avantgarde<>(new Knight());
+      final Army<Knight> knightsAvanRight = new Avantgarde<>(new Knight());
+      final Escort<Man> peasantsEscortBack = new Escort<>(new Peasant());
+      final Escort<Peasant> peasantEscortLeft = new Escort<>(new Peasant());
+      final Army<Peasant> peasantEscortRigth = new Escort<>(new Peasant());
+      final Army<Knight> knigthsReserve = new Escort<>(new Knight());
+      final Avantgarde<Man> raiders = new Avantgarde<>(new Knight());
 
-        doScout(knightsAvanLeft);
-        moveKnights(knightsAvanRight);
-        moveEscort(peasantsEscortBack);
-        moveSomething(peasantEscortLeft);
-        moveSomething(knigthsReserve);
-        moveKnights(knigthsReserve);
-        moveSomething(peasantEscortRigth);
-        moveSomething(raiders);
+      doScout(knightsAvanLeft);
+      moveKnights(knightsAvanRight);
+      moveEscort(peasantsEscortBack);
+      moveSomething(peasantEscortLeft);
+      moveSomething(knigthsReserve);
+      moveKnights(knigthsReserve);
+      moveSomething(peasantEscortRigth);
+      moveSomething(raiders);
     }
-}
+  }
